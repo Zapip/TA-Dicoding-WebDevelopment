61192a2e99969a59a674feebf2e3a42b17a152f8
Zafif Hilmi <zafif3165@gmail.com>
2023-07-05 20:18:24 +0700

    about-skills restore

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ const Navbar = document.querySelector(".header-container .nav-list ul");
 hamburgerToggle.addEventListener("click", function () {
   Navbar.classList.toggle("toggle");
 });
+
+// Smooth scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  for (const link of navLinks) {
+    link.addEventListener("click", clickHandler);
+  }
+
+  function clickHandler(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  }
+});
